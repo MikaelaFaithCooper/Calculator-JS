86f4e25b6d9c15fe98064c5b5c29180a72be3a82
Mikaela Cooper <cooper.mikaelafaith@gmail.com>
2023-05-10 01:24:16 +0800

    UI
    • Buttons added
    • UI centered
    • Added new font
    • Theme/Color scheme changed from original output

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 tailwind.config = {
-    theme: {
-      extend: {
-        colors: {
-          clifford: '#da373d',
-        }
-      }
+  theme: {
+    extend: {
+      colors: {
+        'papayawhip' : '#ffefd4',
+        'terracotta' : '#d15252',
+        'frenchbeige' : '#c2a676',
+        'liver' : '#4d4d4d',
+
+      },
+      fontFamily: {
+          // 'dotgothic16': ['DotGothic16', 'sans-serif'],
+          // 'orbitron': ['Orbitron', 'sans-serif'],
+          'digital7reg': ['Digital7Reg', 'sans-serif']
+      },
     }
   }
+}
